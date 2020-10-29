22434b890b21a681c8dd794b76e06bd8eebf5995
Christian <christian@randleff.dk>
2020-10-29 13:16:17 +0100

    har tilføjet så den sotere det nye data

--- covid19/covid19/src/main/java/DataBroker.java
+++ covid19/covid19/src/main/java/DataBroker.java
@@ -7,16 +7,33 @@ import java.util.ArrayList;
 public class DataBroker<data> extends PApplet {
     PApplet p;
     Table data;
+    Table covidData = new Table();
 
    public ArrayList<Data> Datalist = new ArrayList<Data>();
 
     DataBroker(PApplet p, Table data){
         this.p = p;
         this.data = data;
+
+
+
     }
 
     public void loadData() {
+        covidData.addRow(data.getRow(0));
+      for(int i = 0 ; i < data.getRowCount() ; ++i){
 
+          if(data.getString(i,2).equalsIgnoreCase("Australia")){
+              covidData.addRow(data.getRow(i));
+          }
+
+          for(int j = 0; j< covidData.getRowCount(); ++j){
+              System.out.println(covidData.getString(j,1));
+
+          }
+
+      }
+        /*
         for(int i = 1; i < data.getRowCount(); ++i){
             for(int j = 2; j < data.getColumnCount(); ++j) {
                 String country = data.getString( i ,1);
@@ -26,21 +43,16 @@ public class DataBroker<data> extends PApplet {
                 Datalist.add(myData);
                 println("land: " + myData.name + " år: " + myData.Year + " døde: " + myData.Death);
             }
-        }
+        }*/
     }
-    public int getData(String countryName, int Year){
+  public int getData(String Year){
         int a = 0;
-        for(int i = 0; i < Datalist.size(); ++i) {
-            Data data = Datalist.get(i);
-            if(countryName.equalsIgnoreCase(data.name) && Year == data.Year) {
-                a = Datalist.get(i).Death;
-            }
-        }
+
         return a;
     }
 
-    public ArrayList<Integer> getAllContryDeath(String countryName){
-        ArrayList<Integer> deathList = new ArrayList<Integer>();
+ /*    public ArrayList<Integer> getAllContryDeath(String countryName){
+       ArrayList<Integer> deathList = new ArrayList<Integer>();
 
         for(int i = 0; i < Datalist.size(); ++i) {
             Data data = Datalist.get(i);
@@ -51,7 +63,7 @@ public class DataBroker<data> extends PApplet {
         }
         return deathList;
     }
-
+*/
 
 
 
