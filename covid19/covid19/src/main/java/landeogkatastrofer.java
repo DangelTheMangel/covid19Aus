8c18e66648a3cb3cf83c0c638621bd9ed68ac5c6
Christian <christian@randleff.dk>
2020-11-12 15:46:21 +0100

    jjjefjejf

--- covid19/covid19/src/main/java/landeogkatastrofer.java
+++ covid19/covid19/src/main/java/landeogkatastrofer.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PVector;
 import processing.data.Table;
 
 //Bazinga min guttermand
@@ -8,10 +9,12 @@ DataBroker data;
 String Infofelt = "Land: " + "\nÅR: " +  "\nDøde: ";
 TextFlet year,day, mounths, contry;
 Table table;
+PVector[] chosen = {null, null,null};
+int chosenshow =0;
 Plot plot;
 String ChosenDate = " ";
 String chosenContrey = "Australia";
-AlmindeligKnap btnColonDown, btnColonUp;
+AlmindeligKnap btnColonDown, btnColonUp, btnAdvice,btnGernalfacts,btnGraph;
 ToggleKnap btnOptions;
 int chosenColon = 1;
 int[] graphTopic = {4,7,8,25};
@@ -31,11 +34,24 @@ PieChart chart = new PieChart(this);
         data = new DataBroker(this, table );
 
         data.loadData();
-        year = new TextFlet(this,  width / 24, (int) (height / 12  ), width / 12, height / 12, "Year");
-        day = new TextFlet(this,  width / 24 + width / 12 +10 , (int) (height / 12  )  , width / 12, height / 12, "Day");
-        mounths = new TextFlet(this,  width / 24 + (width / 12 +10)*2, (int) (height / 12  )  , width / 12 , height / 12, "Months");
+        btnGernalfacts = new AlmindeligKnap(this, (width / 3)*2,(int) (height / 12  ) + (height / 12 +20)*1/2 , width / 3, height / 14, "general fackts");
+        btnGraph =new AlmindeligKnap(this, 0,(int) (height / 12  ) + (height / 12 +20)*1/2 , width / 3, height / 14, "insert name");
+        btnAdvice = new AlmindeligKnap(this, width / 3,(int) (height / 12  ) + (height / 12 +20)*1/2 , width / 3, height / 14, "advance");
+        btnOptions = new ToggleKnap(this, 0,(int) (height / 12  ) + (height / 12 +20)*1/2 + height / 14, width / 3, height / 14, "Options");
 
-        contry = new TextFlet(this, width / 24, (int) (height / 12  ) + (height / 12 +20)*1  , (width / 12 +7)*3, height / 12, "Countries");
+        year = new TextFlet(this,  width / 24, (int) (height / 12  ) + (height / 12 +20)*2, width / 12, height / 12, "Year");
+        day = new TextFlet(this,  width / 24 + width / 12 +10 , (int) (height / 12  ) + (height / 12 +20)*2  , width / 12, height / 12, "Day");
+        mounths = new TextFlet(this,  width / 24 + (width / 12 +10)*2, (int) (height / 12  ) + (height / 12 +20)*2 , width / 12 , height / 12, "Months");
+
+        btnColonUp = new AlmindeligKnap(this,width / 24,
+                (int) (height / 12  ) + (height / 12 +20)*4
+                , (width / 24), height / 12, "<");
+
+        btnColonDown = new AlmindeligKnap(this,width / 24 + (width / 12 +7)*3 - (width / 24 )
+                , (int) (height / 12  ) + (height / 12 +20)*4
+                , (width / 24 ), height / 12, ">");
+
+        contry = new TextFlet(this, width / 24, (int) (height / 12  ) + (height / 12 +20)*3  , (width / 12 +7)*3, height / 12, "Countries");
         contry.indput = chosenContrey;
         plot = new Plot(this,width/2 - width/8, height/3, ( width/2), height/4, data);
        // println(data.getData("AFGHANISTAN", 2014));
@@ -43,10 +59,9 @@ PieChart chart = new PieChart(this);
         plot.deathGraph.inputTable(data.covidData);
         plot.pillarChart.inputTable(data.covidData);
 
-        btnColonUp = new AlmindeligKnap(this,width / 24, (int) (height / 12  ) + (height / 12 +20)*2  , (width / 24), height / 12, "<");
-        btnColonDown = new AlmindeligKnap(this,width / 24 + (width / 12 +7)*3 - (width / 24 ), (int) (height / 12  ) + (height / 12 +20)*2  , (width / 24 ), height / 12, ">");
-        btnOptions = new ToggleKnap(this, width / 24,height / 30, width / 20, height / 14, "Options");
-
+        chosen[0] = new PVector(0,(int) (height / 12  ) + (height / 12 +20)*1/2);
+        chosen[1] = new PVector(width/3,(int) (height / 12  ) + (height / 12 +20)*1/2);
+        chosen[2] = new PVector((width/3)*2,(int) (height / 12  ) + (height / 12 +20)*1/2);
 
 
 
@@ -57,20 +72,34 @@ PieChart chart = new PieChart(this);
 
         clear();
         background(200);
-        plot.draw();
-        if(btnOptions.klikket){
-            year.tegnTextFlet();
-            day.tegnTextFlet();
-            mounths.tegnTextFlet();
-            contry.tegnTextFlet();
 
-            text(Infofelt,width - width / 4, height / 12  );
-            text(data.covidData.getString(0,graphTopic[chosenColon]),width / 24 + (width / 12+ 40), (int) (height / 12  ) + (height / 12 +20)*2  );
-            btnColonDown.tegnKnap();
-            btnColonUp.tegnKnap();
+        fill(157, 183, 209);
+        rect(0,0,width,(height / 14)*3);
+        fill(250);
+        rect(chosen[chosenshow].x, chosen[chosenshow].y,width / 3, height / 14);
+        if(chosenshow ==0) {
+            plot.draw();
+            if (btnOptions.klikket) {
+                year.tegnTextFlet();
+                day.tegnTextFlet();
+                mounths.tegnTextFlet();
+                contry.tegnTextFlet();
+
+                text(Infofelt, width / 24 + (width / 12 + 40), (int) (height / 12) + (height / 12 + 20) * 6);
+                text(data.covidData.getString(0, graphTopic[chosenColon]), width / 24 + (width / 12 + 40), (int) (height / 12) + (height / 12 + 20) * 9 / 2);
+                btnColonDown.tegnKnap();
+                btnColonUp.tegnKnap();
+                btnOptions.tegnKnap();
+            }
+        } else if(chosenshow ==1){
+
+        } else if(chosenshow == 2){
 
         }
-        btnOptions.tegnKnap();
+
+        btnGernalfacts.tegnKnap();
+        btnGraph.tegnKnap();
+        btnAdvice.tegnKnap();
 
 
 
@@ -82,15 +111,32 @@ PieChart chart = new PieChart(this);
         System.out.println(graphTopic[chosenColon]);
         plot.clicked(mouseX,mouseY);
         btnOptions.registrerKlik(mouseX,mouseY);
+        btnAdvice.registrerKlik(mouseX,mouseY);
+        btnGraph.registrerKlik(mouseX,mouseY);
+        btnGernalfacts.registrerKlik(mouseX,mouseY);
+        if(btnGraph.klikket){
+            chosenshow = 0;
+            btnGraph.registrerRelease();
+        }
+        if(btnGernalfacts.klikket){
+            chosenshow = 2;
+            btnGernalfacts.registrerRelease();
+        }
+        if(btnAdvice.klikket){
+            chosenshow = 1;
+            btnAdvice.registrerRelease();
+        }
+
+
         if(!btnOptions.klikket) {
-            plot.changeSize(100,200,1000,370,height,width);
+            plot.changeSize(100+ height/14,200+ height/14,1000 ,370- height/14,height,width);
 
         } else {
             year.KlikTjek(mouseX, mouseY);
             day.KlikTjek(mouseX, mouseY);
             mounths.KlikTjek(mouseX, mouseY);
             contry.KlikTjek(mouseX, mouseY);
-            plot.changeSize(width/2 - width/8, height/3, ( width/2), height/4,height,width);
+            plot.changeSize(width/2 - width/8, height/3+ height/14 , ( width/2)- height/14, height/4- height/14,height,width);
 
             btnColonDown.registrerKlik(mouseX, mouseY);
             btnColonUp.registrerKlik(mouseX, mouseY);
